58994b1be7905982aea1ac9d3d71523483020aca
Jesper Granbo <jesper_granbo@hotmail.com>
2018-11-05 14:55:59 +0100

    hemsidan är aktuell

---
+++ Uppgift4/Models/Channel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Uppgift4.Models
+{
+    public class Channel
+    {
+        public string name { get; set; }
+        public DateTime startTime { get; set; }
+        public TimeSpan endTime { get; set; }
+        public string showType { get; set; }
+    }
+}
