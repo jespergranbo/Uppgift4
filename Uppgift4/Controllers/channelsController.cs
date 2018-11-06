3935ca867dbe1db5159a03a512e3c8d267a4a5fd
Jesper Granbo <jesper_granbo@hotmail.com>
2018-11-06 12:44:29 +0100

    xD

---
+++ Uppgift4/Controllers/channelsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Uppgift4.Models;
+
+namespace Uppgift4.Controllers
+{
+    public class channelsController : Controller
+    {
+        private tvtablaEntities2 db = new tvtablaEntities2();
+
+        // GET: channels
+        public ActionResult Index()
+        {
+            return View(db.channel.ToList());
+        }
+
+        // GET: channels/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            channel channel = db.channel.Find(id);
+            if (channel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(channel);
+        }
+
+        // GET: channels/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: channels/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "channel_id,channel_name")] channel channel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.channel.Add(channel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(channel);
+        }
+
+        // GET: channels/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            channel channel = db.channel.Find(id);
+            if (channel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(channel);
+        }
+
+        // POST: channels/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "channel_id,channel_name")] channel channel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(channel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(channel);
+        }
+
+        // GET: channels/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            channel channel = db.channel.Find(id);
+            if (channel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(channel);
+        }
+
+        // POST: channels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            channel channel = db.channel.Find(id);
+            db.channel.Remove(channel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
