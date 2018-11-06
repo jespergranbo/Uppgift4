1a14d297c115172ce435e7efdc5f6d1c33626a46
Jesper Granbo <jesper_granbo@hotmail.com>
2018-11-06 13:43:25 +0100

    view model

---
+++ Uppgift4/Controllers/ChannelProgramDetailsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Uppgift4.Models;
+using Uppgift4.ViewModels;
+
+namespace Uppgift4.Controllers
+{
+    public class ChannelProgramDetailsController : Controller
+    {
+        // GET: ChannelProgramDetails
+        public ActionResult Index()
+        {
+            tvtablaEntities2 tvdb = new tvtablaEntities2();
+            List<ProgramVM> programsVMlist = new List<ProgramVM>();
+            var programList = (from chn in tvdb.channel
+                               join pgn in tvdb.program on chn.channel_id equals pgn.channel_id
+                               select new {chn.channel_id, chn.channel_name, pgn.program_name, pgn.start_time, pgn.end_time, pgn.program_id }).ToList();
+            foreach (var item in programList)
+            {
+                ProgramVM vmItem = new ProgramVM();
+                vmItem.channel_name = item.channel_name;
+                vmItem.program_name = item.program_name;
+                vmItem.channel_id = item.channel_id;
+                vmItem.end_time = item.end_time;
+                vmItem.start_time = item.start_time;
+                vmItem.program_id = item.program_id;
+                programsVMlist.Add(vmItem);
+            }
+
+            return View(programsVMlist);
+        }
+    }
+}
---
+++ Uppgift4/ViewModels/ProgramVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Uppgift4.ViewModels
+{
+    public class ProgramVM
+    {
+        public int channel_id { get; set; }
+        public string channel_name { get; set; }
+        public int program_id { get; set; }
+        public string program_name { get; set; }
+        public DateTime start_time { get; set; }
+        public byte[] end_time { get; set; }
+    }
+}
