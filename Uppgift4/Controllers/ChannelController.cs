9f386ce1e57bbc85244d69f39b345eef98feae80
Jesper Granbo <jesper_granbo@hotmail.com>
2018-11-06 12:52:41 +0100

    .

---
+++ Uppgift4/Controllers/ChannelController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Uppgift4.Models.ChannelOperations;
+
+namespace Uppgift4.Controllers
+{
+    public class ChannelController : Controller
+    {
+        private ChannelOperations co = new ChannelOperations();
+
+        // GET: Channel
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
